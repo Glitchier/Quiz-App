508809cf8eecc56a69ac582e96d51ca8d26af884
Glitchier <pratyushkumar072@gmail.com>
2020-12-25 18:28:00 +0530

    first commit

---
+++ lib/ques.dart
@@ -0,0 +1,13 @@
+class Question {
+  String quesText;
+  bool quesAnswer;
+
+  Question({String q, bool a}) {
+    quesText = q;
+    quesAnswer = a;
+  }
+
+  String get getQuesText => null;
+
+  bool get getAns => null;
+}
